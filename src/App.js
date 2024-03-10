b27aa42b341f21beb86831884cd78f6c561290af
Faizan Fareed Khan <yosoylilnib@gmail.com>
2024-03-10 23:41:38 +0500

    add random color generator functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Accordian from "./components/accordion";
+import { RandomColor } from "./components/randon-color";
 
 function App() {
   return (
-    <div className="App">
-      {/* This is our Accordian Component */}
-
-      <Accordian />
-    </div>
+    <>
+      <div className="App">
+        {/* This is our Accordian Component */}
+        {/* <Accordian /> */}
+        {/* This is Random Color Component */}
+        <RandomColor />
+      </div>
+    </>
   );
 }
 
