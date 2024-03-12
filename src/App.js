82691eb9150bc1e26e2c023af8c7bc93f4d059b3
Faizan Fareed Khan <yosoylilnib@gmail.com>
2024-03-12 12:25:00 +0500

    add image slider functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 // import Accordian from "./components/accordion";
 // import { RandomColor } from "./components/randon-color";
-import StarRating from "./components/star-rating";
+// import StarRating from "./components/star-rating";
+import ImageSlider from "./components/image-slider";
 
 function App() {
   return (
@@ -12,7 +13,13 @@ function App() {
         {/* This is Random Color Component */}
         {/* <RandomColor /> */}
         {/* This is Star Rating Component */}
-        <StarRating noOfStars={10} />
+        {/* <StarRating noOfStars={10} /> */}
+        {/* This is Image Slider Component */}
+        <ImageSlider
+          url={"https://picsum.photos/v2/list"}
+          limit={"10"}
+          page={"1"}
+        />
       </div>
     </>
   );
